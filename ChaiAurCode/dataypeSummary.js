4b8857d78bdd40fdbd23c12a24e9410bc171db94
Krishna <lkrushana@gmail.com>
2024-04-23 18:06:10 +0530

    DatatypeSummary

---
+++ ChaiAurCode/dataypeSummary.js
@@ -0,0 +1,30 @@
+//primitive
+// seven types : String,Number,Boolean,null,undefined,Symbol,Bigint
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn =false;
+const outsideTemp =null;
+let userEmail;
+
+const id =Symbol('123');
+const anotherId =Symbol('123');
+console.log(id===anotherId);
+
+// const bigNumber = 345558532158415n;
+
+//Reference (Non primitive)
+//Array,object,Functions
+//become master in javascript(learn objects,web events or browser events)
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:"hitesh",
+    age: 22,
+}
+const myFunction = function 
+(params) {
+    console.log("Hello World");
+    
+}
+console.log(typeof  bigNumber);
