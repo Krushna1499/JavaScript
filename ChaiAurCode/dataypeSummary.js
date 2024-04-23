cfe6b89f6d8305b46bc8b6ad4dfa7428a14f20b1
Krishna <lkrushana@gmail.com>
2024-04-23 18:28:54 +0530

    few

--- ChaiAurCode/dataypeSummary.js
+++ ChaiAurCode/dataypeSummary.js
@@ -28,3 +28,24 @@ const myFunction = function
     
 }
 console.log(typeof  bigNumber);
+//Stack (Primitive) Heap(Non-Primitive)
+
+let myYoutubename ="hiteshchoudharyditcom"
+
+let anothername=myYoutubename
+anothername="chaiaurcode"
+
+console.log(
+    myYoutubename
+);
+console.log(anothername);
+
+let userOne={
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+ userTwo.email="hitesh@google.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
