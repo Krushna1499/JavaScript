127d8f2010a867b8f6a1dd7e68388ab80fa44a79
Krishna <lkrushana@gmail.com>
2024-04-29 14:46:10 +0530

    StringConcepts

---
+++ DnyaneshwarSir/String_and_methods/script.js
@@ -0,0 +1,13 @@
+var text ="My favourite book is 'Bhagvat Geeta'";
+
+document.getElementById("firstPara").innerHTML = text;
+var firstName="John";
+var lastName="Wick";
+
+var str =`My name is ${firstName} ${lastName},  Addition of two numbers ${2+3}`;
+document.getElementById("para2").innerHTML=str;
+
+let p ="Namrata";
+let lengthofp =p.length;
+console.log(lengthofp);
+
