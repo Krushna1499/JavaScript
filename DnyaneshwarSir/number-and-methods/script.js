12fd97a0ba4189ea8927e0e5c050dd28c8ad9dc3
Krishna <lkrushana@gmail.com>
2024-04-30 16:44:57 +0530

    "Number And Methods"

---
+++ DnyaneshwarSir/number-and-methods/script.js
@@ -0,0 +1,23 @@
+let x=123;
+console.log(x.toString()); 
+
+console.log(x.toFixed(2));
+
+let y=9.656;
+console.log(y.toExponential(2));
+
+//Precision 
+let z =6.66;
+console.log(z.toPrecision(2)); 
+
+//value of
+let a=123;
+console.log(a.valueOf());
+
+//number
+let s ="10";
+console.log(Number(new Date("2000-01-01")));
+
+//parseFloat()
+
+console.log(parseFloat("10 years"));
