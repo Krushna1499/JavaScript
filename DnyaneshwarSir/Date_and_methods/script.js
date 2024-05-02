a66f2aa22e8b311b5715ce5e02bc6b3827383187
Krishna <lkrushana@gmail.com>
2024-05-02 13:23:34 +0530

    Date and it's Methods

---
+++ DnyaneshwarSir/Date_and_methods/script.js
@@ -0,0 +1,21 @@
+let d =new Date(2001,1,1,2);
+console.log(d);
+
+console.log(d.toISOString);
+console.log(d.toUTCString);
+
+console.log(d.getFullYear);
+
+console.log(d.getDate);
+var f = new Date("2021,12,25");
+console.log(f.getFullYear());
+console.log(f.getMonth());
+console.log(f.getDate());
+
+console.log(f.getHours);
+
+const p=new Date()
+
+console.log(p.setFullYear(2002));
+
+
