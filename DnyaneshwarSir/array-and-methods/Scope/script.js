ed22850451965b93413afdfd2c8e9e36caca93cf
Krishna <lkrushana@gmail.com>
2024-05-02 15:09:15 +0530

    Scopeof var,let

---
+++ DnyaneshwarSir/array-and-methods/Scope/script.js
@@ -0,0 +1,19 @@
+let a =10;
+let b =20;
+let c= 30;
+
+function callMe()
+{   let d= 50;     //only in function scope
+    console.log(a,b,c,d);
+    {
+        var r =10;
+     }
+     
+     console.log(d);
+console.log(r);
+}
+
+callMe();
+{
+
+}
